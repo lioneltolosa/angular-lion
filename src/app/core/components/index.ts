d039a1b3466722f49e2819131c38424f8ecc052d
lionelmiguel.tolosa <lionelmiguel.tolosa@altran.es>
2018-09-17 18:50:45 +0200

    ADD Slider component

--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -1,3 +1,4 @@
 export { PreHeaderComponent } from './pre-header/pre-header.component';
 export { FooterComponent } from './footer/footer.component';
 export { HeaderComponent } from './header/header.component';
+export { SliderComponent } from './slider/slider.component';
